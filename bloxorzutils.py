1e2f362f3cf2e89e535ca78bd7b9588830e4e26c
Uğur Mengilli <ugurmengilli@gmail.com>
2018-10-19 18:42:57 +0300

    Add utility module for functioning of others
    Add aima-python module to system path to enable imports.

---
+++ bloxorzutils.py
@@ -0,0 +1,11 @@
+"""
+Author: Ugur Mengilli
+
+Includes statements of corrections, customizations, initialization for correct functioning of the modules.
+"""
+
+import sys
+import os
+
+
+sys.path.append(os.path.join(sys.path[0], 'aima'))
